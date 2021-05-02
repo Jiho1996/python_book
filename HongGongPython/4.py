eb8716882e9c80969be40a417247280ee1edf7de
jiho1996 <jiho.urssu@gmail.com>
2021-05-02 17:34:17 +0900

    혼공파

---
+++ HongGongPython/4.py
@@ -0,0 +1,26 @@
+character = {
+    "name" : "기사",
+    "level" : 12,
+    "items" : {
+        "sword" : "불꽃 검",
+        "armor" : "소울블레이드"},
+    "skill" : ["베기", "물기", "꼬집기"]
+}
+
+for key in character:
+    if type(character[key]) != dict and type(character[key]) != list:
+        print(key ,":",character[key])
+
+    elif type(character[key]) is dict:
+
+        for smallkey in character[key] :
+
+            print(key,":", character["items"][smallkey])
+
+
+    elif type(character[key]) is list :
+
+        for i in range(3) :
+            print(key, ":", character["skill"][i])
+
+
---
+++ venv/this_is_coding_test/rr.py
@@ -0,0 +1,5 @@
+
+names = ['gildong', 'suji', 'minwoo', 'tom', 'jimmy']
+
+for i in range(len(names)):
+    print("index : %d, Name : %s" %(i, names[i]))
